43891ffd9270c5ad2cd4caf478434f3fd9d9c465
Josh Covington <joshpcovington@gmail.com>
2023-10-02 18:31:16 -0600

    changes

--- assets/js/dine-out-script.js
+++ assets/js/dine-out-script.js
@@ -1,31 +1,35 @@
-const API_ID = "YourAPIID"; // Your API ID (if required)
-const API_KEY = "AIzaSyDronXJJHk4f5fXEP71UYplrWNcWUFUNmk"; // Your Google Places API Key
-const API_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json";
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOMContentLoaded event fired.");
 
 // Get references to HTML elements
-const locationSelect = document.getElementById("location");
+const zipCodeInput = document.getElementById("zipCode");
+const foodTypeSelect = document.getElementById("foodType");
 const searchButton = document.getElementById("searchButton");
 const resultsDiv = document.getElementById("results");
 
 // Function to perform a search
-function performSearch() {
-    const searchQuery = "Chinese food";
-    const location = locationSelect.value;
-    const radius = 1000; // Search radius in meters (adjust as needed)
+function performSearch(event) {
+
+    const zipCode = zipCodeInput.value;
+    const foodType = foodTypeSelect.value;
+
+    // Your Google Places API key
+    const API_KEY = "AIzaSyDronXJJHk4f5fXEP71UYplrWNcWUFUNmk";
 
     // Construct the request URL
-    let requestUrl = `${API_URL}?query=${searchQuery}&location=${location}&radius=${radius}&key=${API_KEY}`;
+    const requestUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${foodType}+restaurants&location=${zipCode}&key=${API_KEY}`;
 
-    // If you have an API_ID, you can add it to the URL
-    if (API_ID) {
-        requestUrl += `&client=${API_ID}`;
-    }
+    fetch(requestUrl, {
+        headers: {
+            "Accept-Encoding": "gzip, deflate", // Request compressed response
+        },
+    })
 
     // Make a request to the Google Places API
     fetch(requestUrl)
         .then((response) => response.json())
         .then((data) => {
-            // Handle the API response (list of Chinese restaurants)
+            // Handle the API response (list of restaurants)
             console.log(data);
             // You can process the data to display restaurant names, addresses, etc.
             resultsDiv.innerHTML = JSON.stringify(data, null, 2); // Display JSON data as text
@@ -36,6 +40,8 @@ function performSearch() {
 }
 
 // Event listener for the search button
-searchButton.addEventListener("click", () => {
-    performSearch();
+    searchButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        performSearch();
+    });
 });
