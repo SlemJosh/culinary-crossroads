9eadd4532f0a88b7ecd858fb3dc7399fd2c7b4a5
Josh Covington <joshpcovington@gmail.com>
2023-10-02 20:14:41 -0600

    recipe images and links

--- assets/js/dine-in-script.js
+++ assets/js/dine-in-script.js
@@ -8,31 +8,64 @@ var searchButton = document.querySelector('#searchButton');
 var searchArea = document.querySelector('#searchArea');
 var recipiesArea = document.querySelector('#recipiesDisplay');
 
-
 function handleFormSubmit() {
-    var userInput = inputEl.value
+    var userInput = inputEl.value;
     console.log(userInput)
-    fetchAPI()
+    fetchAPI();
+}
+
+function displayRecipes(recipes) {
+    const recipiesDisplay = document.querySelector('#recipiesDisplay');
+    recipiesDisplay.innerHTML = '';
+    const topRecipes = recipes.slice(0, 6);
+
+    topRecipes.forEach((recipe, index) => {
+        const recipeContainer = document.createElement('div');
+        recipeContainer.classList.add('recipe-container');
+
+        const recipeLink = document.createElement('a');
+        recipeLink.href = recipe.url;
+        recipeLink.target = '_blank'; 
+
+        const recipeImage = document.createElement('img');
+        recipeImage.src = recipe.image;
+        recipeImage.alt = `${recipe.label} Image`;
+        recipeImage.classList.add('recipe-image');
+
+        recipeLink.appendChild(recipeImage);
+        recipeContainer.appendChild(recipeLink);
+
+        const recipeButton = document.createElement('button');
+        recipeButton.textContent = `${index + 1}. ${recipe.label}`;
+        recipeButton.classList.add('recipe-button');
+
+        recipeContainer.appendChild(recipeButton);
+        recipiesDisplay.appendChild(recipeContainer);
+    });
+
+    recipiesDisplay.classList.remove('hidden');
 }
 
 const fetchAPI = function () {
-    var userInput = inputEl.value
-    const queryURL = 'https://api.edamam.com/api/recipes/v2?type=public&q=' + userInput + '&app_id=' + API_ID + '&app_key=' + API_KEY
-    searchArea.classList.add('hidden')
-    recipiesArea.classList.remove('hidden')
+    var userInput = inputEl.value;
+    const queryURL = 'https://api.edamam.com/api/recipes/v2?type=public&q=' + userInput + '&app_id=' + API_ID + '&app_key=' + API_KEY;
+    searchArea.classList.add('hidden');
+    recipiesArea.classList.remove('hidden');
 
     fetch(queryURL)
-      .then((response) => {
-        if (response.ok) {
-            response.json().then((data) => {
-            console.log(data)
-
+        .then((response) => {
+            if (response.ok) {
+                response.json().then((data) => {
+                    const recipes = data.hits.map(hit => hit.recipe);
+                    displayRecipes(recipes);
+                });
+            } else {
+                console.error('API error:', response.status, response.statusText);
+            }
         })
-    }}
-)};
-
-
-
+        .catch((error) => {
+            console.error('Network error:', error);
+        });
+};
 
 searchButton.addEventListener('click', handleFormSubmit);
- 
